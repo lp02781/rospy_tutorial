ba700c9bbf931afe9109c1ee35e068cfdb203fe9
lp02781 <lp02781@gmail.com>
2021-07-15 00:34:57 +0700

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mfikih15/rospy_tutorial/src
+# Build directory: /home/mfikih15/rospy_tutorial/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("pypy")
---
+++ build/pypy/CMakeFiles/haha.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/haha.dir/src/haha.cpp.o"
+  "/home/mfikih15/rospy_tutorial/devel/lib/pypy/haha.pdb"
+  "/home/mfikih15/rospy_tutorial/devel/lib/pypy/haha"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/haha.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
